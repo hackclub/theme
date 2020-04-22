69da577d516261165d878ebf6d093df5f78adb9c
Lachlan Campbell <lachlan@hackclub.com>
2020-04-22 01:23:52 -0400

    Add larger font sizes

--- packages/theme/src/index.js
+++ packages/theme/src/index.js
@@ -26,9 +26,9 @@ const colors = {
 }
 
 const theme = {
-  breakpoints: [32, 48, 64, 96, 128].map(w => `${w}em`),
+  breakpoints: [32, 48, 64, 96, 128].map((w) => `${w}em`),
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
-  fontSizes: [12, 16, 20, 24, 32, 48, 64, 96, 128],
+  fontSizes: [12, 16, 20, 24, 32, 48, 64, 96, 128, 160, 192],
   initialColorModeName: 'light',
   useColorSchemeMediaQuery: true,
   colors: {
