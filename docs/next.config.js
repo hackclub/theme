56e29275fa7f770373da9329eda9074d1ba177df
Lachlan Campbell <lachlan@hackclub.com>
2020-04-22 01:28:05 -0400

    Add brand page to docs

---
+++ docs/next.config.js
@@ -0,0 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  assetPrefix: isProd ? 'https://theme.hackclub.com' : ''
+})
