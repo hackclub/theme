bf40f7f84dfce88657a8a179838fdaeeb926cbc3
Lachlan Campbell <lachlan@hackclub.com>
2020-05-22 18:30:40 +0000

    Meta 1.0.0

--- packages/meta/README.md
+++ packages/meta/README.md
@@ -1,6 +1,9 @@
 # @hackclub/meta
 
 React component for generating Open Graph/etc meta tags for Hack Club pages.
+Designed for Next.js but framework-agnostic.
+
+Not recommended for non-Hack Club sites—you’ll get Hack Club favicons :)
 
 ## Usage
 
@@ -12,7 +15,10 @@ yarn add @hackclub/meta
 Example for [hackathons.hackclub.com](https://hackathons.hackclub.com):
 
 ```js
+// import Head from 'next/head'
+
 <Meta
+  as={Head} // component to wrap tags in, defaults to React.Fragment
   name="Hack Club" // site name
   title="Hackathons" // page title
   description="List of upcoming high school hackathons" // page description
@@ -22,6 +28,7 @@ Example for [hackathons.hackclub.com](https://hackathons.hackclub.com):
 />
 ```
 
-All props are optional.
+All props are optional. If you include multiple times, the tags from the last
+instance will be used.
 
 MIT License
--- packages/meta/src/index.js
+++ packages/meta/src/index.js
@@ -4,6 +4,7 @@ const makeTitle = (title, name) =>
   title === name ? title : `${title} – ${name}`
 
 const Meta = ({
+  as: ElementType = React.Fragment,
   name = 'Hack Club', // site name
   title = 'Hack Club', // page title
   description,
@@ -11,34 +12,34 @@ const Meta = ({
   color = '#ec3750',
   manifest = 'https://assets.hackclub.com/favicons/site.webmanifest'
 }) => (
-  <React.Fragment>
+  <ElementType>
+    <meta key="og_locale" property="og:locale" content="en_US" />
     <meta key="og_type" property="og:type" content="website" />
     <meta key="og_site" property="og:site_name" content={name} />
     <meta key="tw_site" name="twitter:site" content="@hackclub" />
-
-    <title>{makeTitle(title, name)}</title>
+    <title key="title">{makeTitle(title, name)}</title>
     <meta key="og_title" property="og:title" content={makeTitle(title, name)} />
     <meta
       key="tw_title"
       name="twitter:title"
       content={makeTitle(title, name)}
     />
-
-    {description && [
-      <meta key="desc" name="description" content={description} />,
-      <meta key="og_desc" property="og:description" content={description} />,
-      <meta key="tw_desc" name="twitter:description" content={description} />
-    ]}
-
-    {image && [
-      <meta key="og_img" property="og:image" content={image} />,
-      <meta key="tw_card" name="twitter:card" content="summary_large_image" />,
-      <meta key="tw_img" name="twitter:image" content={image} />
-    ]}
-
+    {description && (
+      <React.Fragment>
+        <meta key="desc" name="description" content={description} />
+        <meta key="og_desc" property="og:description" content={description} />
+        <meta key="tw_desc" name="twitter:description" content={description} />
+      </React.Fragment>
+    )}
+    {image && (
+      <React.Fragment>
+        <meta key="og_img" property="og:image" content={image} />
+        <meta key="tw_card" name="twitter:card" content="summary_large_image" />
+        <meta key="tw_img" name="twitter:image" content={image} />
+      </React.Fragment>
+    )}
     <meta key="theme_color" name="theme-color" content={color} />
     <meta key="tile_color" name="msapplication-TileColor" content={color} />
-
     <link
       key="safari_icon"
       rel="mask-icon"
@@ -65,9 +66,8 @@ const Meta = ({
       sizes="16x16"
       href="https://assets.hackclub.com/favicons/favicon-16x16.png"
     />
-
     {manifest && <link key="manifest" rel="manifest" href={manifest} />}
-  </React.Fragment>
+  </ElementType>
 )
 
 export default Meta
