c2509f74b67006b1bf215c2972104e309627bff2
Aram <me@aram.sh>
2024-04-17 18:14:06 -0700

    Update index.js

--- packages/meta/src/index.js
+++ packages/meta/src/index.js
@@ -48,10 +48,10 @@ const Meta = ({
       color={color}
     />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png">
-    <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://assets.hackclub.com/favicons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://assets.hackclub.com/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="https://assets.hackclub.com/favicons/favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="https://assets.hackclub.com/favicons/favicon-64x64.png">
     {manifest && <link key="manifest" rel="manifest" href={manifest} />}
     {children}
   </ElementType>
