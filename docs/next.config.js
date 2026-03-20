fc834cc56226baa7a41c5288c95966c0d57a8ecd
Echo <github@3kh0.net>
2026-03-20 11:08:09 -0400

    fix docs builds

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
 const isProd = process.env.VERCEL_ENV === 'production'
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+const monorepoRoot = path.resolve(__dirname, '..')
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
   assetPrefix: isProd ? 'https://theme.hackclub.com' : '',
+  outputFileTracingRoot: monorepoRoot,
   turbopack: {
-    root: path.resolve(__dirname)
-  }
+    root: monorepoRoot
+  },
+  transpilePackages: ['@hackclub/theme', '@hackclub/meta']
 })
